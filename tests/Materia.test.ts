a9773aee882bd6c853b64e4243dc784f4c937b84
Fabricio Moyano <fabricx150@gmail.com>
2026-08-18 18:07:54 -0300

    Clase materia testeada con atributos titular y adjunto

--- tests/Materia.test.ts
+++ tests/Materia.test.ts
@@ -1,21 +1,41 @@
 import { describe, expect, test } from "vitest";
 import { Materia } from "../src/Materia";
+import { Profesor } from "../src/Profesor";
 
 describe("Materia", () => {
 
-    test("Un Materia con nombre", () => {
-    const materia = new Materia("Paradigmas", 2, "ISIS");
+    test("Una materia debe tener un profesor titular", () => {
+    const titular = new Profesor("Carlos Gómez", 101);
+    const adjunto = new Profesor("Laura Pérez", 102);
+    const materia = new Materia("Paradigmas", 2, "ISIS", titular, adjunto);
+    expect(materia.titular).toBe(titular)
+    });
+
+    test("Una materia debe tener un profesor adjunto", () => {
+    const titular = new Profesor("Carlos Gómez", 101);
+    const adjunto = new Profesor("Laura Pérez", 102);
+    const materia = new Materia("Paradigmas", 2, "ISIS", titular, adjunto);
+    expect(materia.adjunto).toBe(adjunto)
+    });
+
+    test("Una Materia debe tener nombre", () => {
+    const titular = new Profesor("Carlos Gómez", 101);
+    const adjunto = new Profesor("Laura Pérez", 102);
+    const materia = new Materia("Paradigmas", 2, "ISIS", titular, adjunto);
     expect(materia.nombre).toBe("Paradigmas")
     });
 
-    test("Un Materia con el legajo 18", () => {
-    const materia = new Materia("Paradigmas", 2, "ISIS");
+    test("Un Materia año", () => {
+    const titular = new Profesor("Carlos Gómez", 101);
+    const adjunto = new Profesor("Laura Pérez", 102);
+    const materia = new Materia("Paradigmas", 2, "ISIS", titular, adjunto);
     expect(materia.anio).toBe(2)
     });
 
-    test("Un Materia con el legajo 18", () => {
-    const materia = new Materia("Paradigmas", 2, "ISIS");
+    test("Un Materia debe tener una carrera", () => {
+    const titular = new Profesor("Carlos Gómez", 101);
+    const adjunto = new Profesor("Laura Pérez", 102);
+    const materia = new Materia("Paradigmas", 2, "ISIS", titular, adjunto);
     expect(materia.carrera).toBe("ISIS")
     });
-
 })
