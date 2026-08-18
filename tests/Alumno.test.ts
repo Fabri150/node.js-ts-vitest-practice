f6c7a3f6abb07efbe1a42c2075c56477785ec37b
Fabricio Moyano <fabricx150@gmail.com>
2026-08-18 17:22:23 -0300

    Nueva clase profesor y testeada

--- tests/Alumno.test.ts
+++ tests/Alumno.test.ts
@@ -3,34 +3,38 @@ import { Alumno } from "../src/Alumno";
 
 describe("Alumno", () => {
 
-  test("un alumno de 18 años y del legajo 10 debe ser mayor de edad", () => {
+  test("Un alumno con el legajo 18", () => {
+  const alumno = new Alumno("Juan", 18, 10);
+  expect(alumno.legajo).toBe(10)
+  });
+
+  test("un alumno de 18 años debe ser mayor de edad", () => {
     const alumno = new Alumno("Juan", 18, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
-  test("un alumno de 25 años y del legajo 10 debe ser mayor de edad", () => {
+  test("un alumno de 25 años debe ser mayor de edad", () => {
     const alumno = new Alumno("Ana", 25, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
-  test("un alumno de 17 años y del legajo 10 no debe ser mayor de edad", () => {
+  test("un alumno de 17 años no debe ser mayor de edad", () => {
     const alumno = new Alumno("Pedro", 17, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(false);
   });
 
-  test("un alumno de 16 años y del legajo 10 debe decir que es menor de edad", () => {
+  test("un alumno de 16 años debe decir que es menor de edad", () => {
     const alumno = new Alumno("Fabri", 16, 10)
 
     expect(alumno.obtenerEstado()).toBe("Menor de edad")
   })
 
-    test("un alumno de 20 años y del legajo 10 debe decir que es mayor de edad", () => {
+    test("un alumno de 20 años debe decir que es mayor de edad", () => {
     const alumno = new Alumno("Pirulo", 20, 10)
 
     expect(alumno.obtenerEstado()).toBe("Mayor de edad")
   })
-
 });
---
+++ tests/Profesor.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect, test } from "vitest";
+import { Profesor } from "../src/Profesor";
+
+describe("Profesor", () => {
+
+    test("Un Profesor con el legajo 18", () => {
+    const profesor = new Profesor("Juan", 18);
+    expect(profesor.nombre).toBe("Juan")
+    });
+
+    test("Un Profesor con el legajo 18", () => {
+    const profesor = new Profesor("Juan", 18);
+    expect(profesor.legajo).toBe(18)
+    });
+
+})
