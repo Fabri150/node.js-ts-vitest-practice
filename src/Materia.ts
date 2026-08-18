ce3cd7868bff878240a99824056cd03971bd7401
Fabricio Moyano <fabricx150@gmail.com>
2026-08-18 17:36:53 -0300

    Materia tiene profesor adjunto titular y lista de alumnos

--- src/Materia.ts
+++ src/Materia.ts
@@ -1,7 +1,18 @@
+import { Profesor } from "../src/Profesor";
+import { Alumno } from "../src/Alumno";
+
 export class Materia {
+    public alumnos: Alumno[] = [];
+
     constructor(
     public nombre: string,
     public anio: number,
-    public carrera: string
+    public carrera: string,
+    public titular: Profesor,
+    public adjunto: Profesor
     ) {}
+
+    agregarAlumno(alumno: Alumno): void {
+        this.alumnos.push(alumno);
+    }
 }
