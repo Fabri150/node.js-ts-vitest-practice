fa43b75390a7fcbb3013ba40e54445941b1dd028
Fabricio Moyano <fabricx150@gmail.com>
2026-08-18 16:58:59 -0300

    Nuevo atributo legajo y testeado

--- tests/Alumno.test.ts
+++ tests/Alumno.test.ts
@@ -3,32 +3,32 @@ import { Alumno } from "../src/Alumno";
 
 describe("Alumno", () => {
 
-  test("un alumno de 18 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Juan", 18);
+  test("un alumno de 18 años y del legajo 10 debe ser mayor de edad", () => {
+    const alumno = new Alumno("Juan", 18, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
-  test("un alumno de 25 años debe ser mayor de edad", () => {
-    const alumno = new Alumno("Ana", 25);
+  test("un alumno de 25 años y del legajo 10 debe ser mayor de edad", () => {
+    const alumno = new Alumno("Ana", 25, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(true);
   });
 
-  test("un alumno de 17 años no debe ser mayor de edad", () => {
-    const alumno = new Alumno("Pedro", 17);
+  test("un alumno de 17 años y del legajo 10 no debe ser mayor de edad", () => {
+    const alumno = new Alumno("Pedro", 17, 10);
 
     expect(alumno.esMayorDeEdad()).toBe(false);
   });
 
-  test("un alumno de 16 años debe decir que es menor de edad", () => {
-    const alumno = new Alumno("Fabri", 16)
+  test("un alumno de 16 años y del legajo 10 debe decir que es menor de edad", () => {
+    const alumno = new Alumno("Fabri", 16, 10)
 
     expect(alumno.obtenerEstado()).toBe("Menor de edad")
   })
 
-    test("un alumno de 20 años debe decir que es mayor de edad", () => {
-    const alumno = new Alumno("Pirulo", 20)
+    test("un alumno de 20 años y del legajo 10 debe decir que es mayor de edad", () => {
+    const alumno = new Alumno("Pirulo", 20, 10)
 
     expect(alumno.obtenerEstado()).toBe("Mayor de edad")
   })
